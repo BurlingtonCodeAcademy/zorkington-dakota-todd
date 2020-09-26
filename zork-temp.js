2ef3f8c790719daca1ba5660fe3c1d9ff400e55c
toddkowalczyk <toddmkowalczyk@gmail.com>
2020-09-25 23:28:04 -0400

    added collection objects, refactored objects to use collection obj, and added boundry check to move

--- zork-temp.js
+++ zork-temp.js
@@ -1,9 +1,79 @@
+class House {
+    constructor() {
+        this.roomCollection = [];
+        this.northEdge = 0;
+        this.southEdge = 0;
+        this.eastEdge = 0;
+        this.westEdge = 0;
+    }
+
+    addRoom(room) {
+        this.roomCollection.push(room);
+    }
+
+    // helper function to get room based on column and row
+    getRoom(col, row) {
+        let currRoom = null;
+
+        this.roomCollection.forEach((room) => {
+            if (col === room.column &&
+                row === room.row) {
+                currRoom = room;        
+            }
+        })
+        return currRoom;
+    }
+
+    getEntryRoom() {
+        return this.roomCollection[0];
+    }
+}
+
+class Room {
+    constructor(roomNumber, column, row, name, description, locked) {
+        this.roomNumber = roomNumber;
+        this.column = column;
+        this.row = row;
+        this.name = name;
+        this.description = description;
+        this.locked = locked;
+    }
+
+    unlock() {
+        this.locked = false;
+    }
+
+    isLocked() {
+        return this.locked;
+    }
+
+}
+
+class ItemCollection {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    getItemCollection() {
+        return this.items;
+    }
+}
+
 class Item {
     constructor(roomNumber, name, desc, takeable) {
-        this.roomNumber = this.roomNumber;
+        this.roomNumber = roomNumber;
         this.name = name;
         this.description = desc;
         this.takeable = false;
+        this.itemStatus = 'in room'
+    }
+
+    markItemTaken() {
+        this.itemStatus = 'taken';
     }
 }
 
@@ -13,39 +83,65 @@ class Player {
         this.name = name;
         this.inventory = [];
         this.status = ['healthy', 'full'];
-        this.currentRoom = 1;
+        this.currentRoom = house.getEntryRoom();
         this.currentColumn = 1;
         this.currentRow = 1;
     }
+
+    // get number of takeable items that a player has in their inventory from a particlar room
+    countRoomItems(roomNumber) {
+        let roomItemCount = 0;
+
+        for (const item of this.inventory) {
+            if (item.roomNumber === roomNumber) {
+                roomItemCount += 1;
+
+            }
+        }
+        return roomItemCount;
+    }
+
+    hasRequiredItems() {
+        return true;
+    }
+
     // Move from room to room
     move(direction) {
+
+        let newColumn = this.currentColumn;
+        let newRow = this.currentRow;
+
         switch (direction) {
             case 'north':
-                this.currentRow -= 1;
+                newRow = this.currentRow - 1;
                 break;
             case 'south':
-                this.currentRow += 1;
+                newRow = this.currentRow + 1; 
                 break;
             case 'east':
-                this.currentColumn += 1;
+                newColumn = this.currentColumn + 1;
                 break;
             case 'west':
-                this.currentColumn -= 1;
+                newColumn = this.currentColumn - 1;
                 break;
             default:
                 console.log('Direction must be north, south, east, or west.')
                 break;
         }
 
-    }
-    // Add item into player inventory
-    addItem(item) {
-        this.inventory.push(item);
+        if (house.getRoom(newColumn, newRow) === null) {
+            console.log("you can\'t go in that directon");
+        }
+        else {
+            this.currentColumn = newColumn;
+            this.currentRow = newRow;
+            this.currentRoom = house.getRoom(newColumn, newRow);
+        }
     }
 
     inspect(target) {
         let element = 0;
-        
+
         for (const property in items) {
             //console.log('target: '+ target + ' items ' + items[element].name);
             if (target === items[element].name) {
@@ -57,54 +153,34 @@ class Player {
 
     // Display room description
     observe() {
-        for (const element of roomMap) {
-
-            if (element.column === this.currentColumn &&
-                element.row === this.currentRow) {
-                console.log(element.description);
-            }
-        }
+        console.log(this.currentRoom.description + '\n');
     }
 
     // Show current inventory items
     showInventory() {
-        for (const element of this.inventory)
-            console.log(element + '\n')
-
+        for (const item of this.inventory)
+            console.log(item.name)
     }
 
     // Take item from room & add to inventory
-    take(item) {
+    take(itemWanted) {
         let index = 0;
-        for (const element of roomMap[this.currentRoom - 1].takeableItems) {
+        for (const item of itemCollection.getItemCollection()) {
             index += 1;
-            if (element === item) {
-                this.addItem(item);
-                roomMap[this.currentRoom - 1].takeableItems = roomMap[this.currentRoom - 1].takeableItems.slice(index)
 
-
+            // Only allow a player to take an item if it is in room they are in
+            // and only allow player to take an item from a room once.
+            if (item.name === itemWanted &&
+                this.currentRoom.roomNumber === item.roomNumber &&
+                item.itemStatus === 'in room') {
+                this.inventory.push(item);
+                item.markItemTaken();
+                console.log(itemWanted + ' added item to inventory');
             }
-
-        }
-    }
-
-}
-class Room {
-    constructor(roomNumber, column, row, name, description, takeableItems) {
-        this.roomNumber = roomNumber;
-        this.column = column;
-        this.row = row;
-        this.name = name;
-        this.description = description;
-        this.takeableItems = takeableItems
-    }
-    // Show items available to add to inventory in each room
-    showItems() {
-        for (const element of this.takeableItems) {
-            console.log(element);
         }
     }
 }
+
 const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
@@ -113,50 +189,71 @@ function ask(questionText) {
         readlineInterface.question(questionText, resolve);
     });
 }
-let roomMap = [];
-let items = [];
 
-start();
+function showIntro() {
+    console.log('__________________________________________________________________________________')
+    console.log('Last night was an epic night out with your friends exploring the Burlington scene.');
+    console.log('Unfortunately for you, in this case, epic got you into trouble!');
+    console.log('You\'ve awoken in a dark, unfamiliar house with no memory of how you ended up there.');
+    console.log('The house looks like it\'s been abandoned for quite some time.\n');
+    console.log('You must navigate through the house to find your escape.');
+    console.log('BEWARE as there are many perils and surprises that await you!');
+
+    // show list of available commands
+    showCommands();
+}
+
+function showCommands() {
+    console.log('You can use the following commands to help you find your way out:\n');
+    console.log('    move north:  to walk north in a room.');
+    console.log('    move south:  to walk south in a room.');
+    console.log('    move east:   to walk east in a room.');
+    console.log('    move west:   to walk west in a room.');
+    console.log('    observe:     to see a description of the room you are currently in.');
+    console.log('    inventory:   to see a list of the items that are currently in your possession.');
+    console.log('    take:        to pick an item in a room up.');
+    console.log('    use:         to use an item that you have in your possession.');
+    console.log('    help:        to see this list of commands again.\n');
+}
 
 async function start() {
-    console.log('You\'ve awoken in a dark, unfamiliar house with no memory of how you ended up there.\nThe house looks like it\'s been abandoned for quite some time.\nYou must navigate through the house to find your escape!');
-    console.log('To find your way through the house & escape to freedom type the following commands:');
-    console.log('North: move north\nSouth: move south\nEast: move east\nWest: move west');
-    console.log('Observe: to see a description of your current room');
-    console.log('Inspect: to read a description of an item in your inventory');
-    console.log('Take: to take an item from a room and add it to inventory');
-    console.log('Use: to use an item from your inventory');
 
+    // Show the Intro Text
+    showIntro();
 
+    //Create house and rooms
 
-    //Create new rooms with descriptions & obtainable items
-    // room number, column, row, room name, room description, takeable items in room
-    roomMap.push(new Room(1, 1, 1, 'library', 'You are in the library.\nThis room has bookcases from floor to ceiling, except one in particular that\'s only a little taller than you are. There\'s a single small desk in the center of the room.',['note', 'flashlight', 'book']))
-    roomMap.push(new Room(2, 2, 3, 'study', 'You are in the study.\nThere is a large oak desk in the room with faded portraits hanging on the wall, one seems to be askew...', ['batteries', 'key']))
-    roomMap.push(new Room(3, 2, 1, 'kitchen', 'You are in the kitchen.\nThe kitchen looks to have been ransacked some time ago.\nMost of the drawers are hanging open with nothing inside.\nThe door to the dining room seems to be locked from the other side.',['knife']))
-    roomMap.push(new Room(4, 2, 2, 'dining room', 'You are in the dining room.\nThere is a long wooden dinner table with several empty viles atop it.\nOne empty vial for every seat at the table ...exept one.', ['vial']));
+    house = new House();
 
-    // Create item objects with their description
-    // room number, item name, item describle, takeable Y/N
+    // room number, column, row, room name, room description, locked boolean 
+    house.addRoom(new Room(1, 1, 1, 'library', 'You are in the library.\nThis room has bookcases from floor to ceiling, except one in particular that\'s only a little taller than you are. There\'s a single small desk in the center of the room.', true))
+    house.addRoom(new Room(2, 2, 1, 'kitchen', 'You are in the kitchen.\nThe kitchen looks to have been ransacked some time ago.\nMost of the drawers are hanging open with nothing inside.\nThe door to the dining room seems to be locked from the other side.', true))
+    house.addRoom(new Room(3, 2, 2, 'dining room', 'You are in the dining room.\nThere is a long wooden dinner table with several empty viles atop it.\nOne empty vial for every seat at the table ...exept one.', true))
+    house.addRoom(new Room(4, 2, 3, 'study', 'You are in the study.\nThere is a large oak desk in the room with faded portraits hanging on the wall,\none seems to be askew...', true))
+
+    // Create items that exist in rooms in house
+    itemCollection = new ItemCollection();
 
     //takeable items
-    items.push(new Item(1, 'note', 'some note', true));
-    items.push(new Item(1, 'flashlight', 'some flashlight', true));
-    items.push(new Item(1, 'book', 'some book', true));
-    items.push(new Item(2, 'batteries', 'some batteries', true));
-    items.push(new Item(2, 'key', 'some key', true));
-    items.push(new Item(3, 'knife', 'some knife', true));
-    items.push(new Item(4, 'vile', 'some vile', true));
+    // room number, item name, item description, takeable boolean
+    itemCollection.addItem(new Item(1, 'note', 'some note', true));
+    itemCollection.addItem(new Item(1, 'flashlight', 'some flashlight', true));
+    itemCollection.addItem(new Item(1, 'book', 'some book', true));
+    itemCollection.addItem(new Item(2, 'batteries', 'some batteries', true));
+    itemCollection.addItem(new Item(2, 'key', 'some key', true));
+    itemCollection.addItem(new Item(3, 'knife', 'some knife', true));
+    itemCollection.addItem(new Item(4, 'vile', 'some vile', true));
 
     // untakeable items
-    items.push(new Item(1, 'desk', 'a mahagony desk with a note, flashlight and book on it', false));
-    items.push(new Item(1, 'bookcase', 'a towerinb bookcase', false));
-    items.push(new Item(2, 'small desk', 'a small desk', false));
-    items.push(new Item(2, 'portrait', 'a portrait of a beautful woman', false));
-    items.push(new Item(3, 'drawer', 'cabinet drawer', false));
-    items.push(new Item(4, 'table', 'an oval dining table', false));
+    itemCollection.addItem(new Item(1, 'desk', 'a mahagony desk with a note, flashlight and book on it', false));
+    itemCollection.addItem(new Item(1, 'bookcase', 'a towerinb bookcase', false));
+    itemCollection.addItem(new Item(2, 'small desk', 'a small desk', false));
+    itemCollection.addItem(new Item(2, 'portrait', 'a portrait of a beautful woman', false));
+    itemCollection.addItem(new Item(3, 'drawer', 'cabinet drawer', false));
+    itemCollection.addItem(new Item(4, 'table', 'an oval dining table', false));
 
-    let player = new Player('Dakota');
+    let player = new Player();
+    player.observe();
 
     while (true) {
         let answer = await ask('>_');
@@ -171,23 +268,37 @@ async function start() {
         if (action === 'exit') {
             console.log('Thanks for playing');
             process.exit();
-        } else if (action === 'north' || action === 'south' || action === 'east' || answer === 'west') {
-            player.move(target);
-        } else if (action === 'observe') {
+        }
+        else if (action === 'move') {
+
+            if (player.hasRequiredItems()) {
+                player.move(target)
+                player.observe();
+            }
+            else {
+                console.log('Sorry, you don\'t have the items you need to move from this room yet');
+            }
+        }
+        else if (action === 'observe') {
             player.observe();
         }
-        else if (action === 'inspect')
-        {
+        else if (action === 'inspect') {
             player.inspect(target);
-        } 
+        }
         else if (action === 'take') {
             player.take(target);
-        } 
-        else if (action == 'inventory') {
+        }
+        else if (action === 'inventory') {
             player.showInventory();
-        } else {
+        }
+        else if (action === 'help') {
+            showCommands();
+        }
+        else {
             console.log('I don\'t understand that command.  Please try again.\n');
         }
     }
-
 }
+
+// Mainline
+start();
